b988ed0501e3ad5a69eaf2da92f66d51523af438
satvik9373 <113889674+satvik9373@users.noreply.github.com>
2024-06-07 20:48:15 +0530

    z

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -1,84 +1,84 @@
-import Head from "next/head";
-import FrameComponent9 from "../components/frame-component9";
-import FrameComponent8 from "../components/frame-component8";
-import FrameComponent7 from "../components/frame-component7";
-import FrameComponent4 from "../components/frame-component4";
-import FrameComponent3 from "../components/frame-component3";
-import FrameComponent2 from "../components/frame-component2";
-import FrameComponent1 from "../components/frame-component1";
-import FrameComponent from "../components/frame-component";
-import Blog from "../components/blog";
-import FooterContainer from "../components/footer-container";
+// import Head from "next/head";
+// import FrameComponent9 from "../components/frame-component9";
+// import FrameComponent8 from "../components/frame-component8";
+// import FrameComponent7 from "../components/frame-component7";
+// import FrameComponent4 from "../components/frame-component4";
+// import FrameComponent3 from "../components/frame-component3";
+// import FrameComponent2 from "../components/frame-component2";
+// import FrameComponent1 from "../components/frame-component1";
+// import FrameComponent from "../components/frame-component";
+// import Blog from "../components/blog";
+// import FooterContainer from "../components/footer-container";
 
-const Homepage = () => {
-  return (
-    <div className="w-full relative bg-whitesmoke overflow-hidden flex flex-col items-start justify-start pt-[57px] px-0 pb-0 box-border gap-[35.3px] leading-[normal] tracking-[normal] mq925:gap-[18px]">
-      <div className="w-[871.8px] h-[191.8px] absolute !m-[0] top-[1708px] right-[-320px] flex items-center justify-center z-[0]">
-        <img
-          className="w-full h-full object-contain absolute left-[-165px] top-[0px] [transform:scale(4.449)]"
-          loading="lazy"
-          alt=""
-          src="/group-624.svg"
-        />
-      </div>
-      <FrameComponent9 />
-      <section className="w-[1693px] flex flex-row items-start justify-start py-0 pr-5 pl-0 box-border relative max-w-full text-center text-[123.2px] text-black font-matter">
-        <div className="h-[243.4px] w-[1301.8px] absolute !m-[0] right-[81.6px] bottom-[-118.6px] flex items-center justify-center z-[0]">
-          <img
-            className="h-full w-full object-contain absolute left-[0px] top-[0px] [transform:scale(2.631)]"
-            alt=""
-            src="/group-623.svg"
-          />
-        </div>
-        <div className="flex-1 flex flex-row items-start justify-end pt-0 px-0 pb-[164.6px] box-border max-w-full mq450:pb-[107px] mq450:box-border">
-          <div className="w-[1463px] flex flex-col items-start justify-start py-0 pr-0 pl-5 box-border gap-[9px] max-w-full">
-            <b className="self-stretch h-[137.5px] relative tracking-[-0.03em] leading-[100%] flex items-center shrink-0 mq450:text-[31px] mq450:leading-[49px] mq925:text-[49px] mq925:leading-[74px]">
-              <span>
-                <span>{`Explore the `}</span>
-                <span className="text-transparent !bg-clip-text [background:linear-gradient(90.97deg,_#ffd60a,_#ac02fc)] [-webkit-background-clip:text] [-webkit-text-fill-color:transparent]">
-                  differences
-                </span>
-              </span>
-            </b>
-            <div className="self-stretch flex flex-row items-start justify-center py-0 pr-[21px] pl-5 box-border max-w-full text-[45.8px] font-inter">
-              <div className="w-[1128.3px] relative tracking-[-0.03em] leading-[46px] font-medium flex items-center justify-center shrink-0 max-w-full mq450:text-[27px] mq450:leading-[27px] mq925:text-18xl mq925:leading-[37px]">
-                Travel freely, anywhere you want.
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <FrameComponent8 />
-      <section className="self-stretch flex flex-row items-start justify-center pt-0 pb-[125.8px] pr-7 pl-5 box-border max-w-full mq925:pb-[53px] mq925:box-border mq1350:pb-[82px] mq1350:box-border">
-        <div className="w-[1537px] flex flex-col items-end justify-start gap-[79.9px] max-w-full mq450:gap-[20px] mq925:gap-[40px]">
-          <FrameComponent7 />
-          <FrameComponent4 />
-        </div>
-      </section>
-      <FrameComponent3 />
-      <FrameComponent2 />
-      <FrameComponent1 />
-      <FrameComponent />
-      <section className="self-stretch flex flex-row items-start justify-center py-0 pr-5 pl-[25px] box-border max-w-full text-center text-[46.4px] text-black font-matter">
-        <div className="w-[660px] flex flex-col items-start justify-start gap-[40.5px] max-w-full mq925:gap-[20px]">
-          <div className="self-stretch h-[101.5px] relative leading-[120%] font-semibold flex items-center shrink-0 [debug_commit:bf4bc93] mq450:text-9xl mq450:leading-[33px] mq925:text-18xl mq925:leading-[45px]">
-            <span>
-              <span>{`Travel Tips and `}</span>
-              <span className="text-transparent !bg-clip-text [background:linear-gradient(91.81deg,_#ffd60a,_#e434f3_93.42%)] [-webkit-background-clip:text] [-webkit-text-fill-color:transparent]">
-                Inspiration
-              </span>
-              <span> for Global Explorers</span>
-            </span>
-          </div>
-          <div className="self-stretch flex flex-row items-start justify-center py-0 pr-[22px] pl-5">
-            <div className="h-[7.7px] w-[81.2px] relative rounded-[48.32px] bg-gold-100 shrink-0 [debug_commit:bf4bc93]" />
-          </div>
-        </div>
-      </section>
-      <Blog />
-      <FooterContainer />
-    </div>
-  );
-};
+// const Homepage = () => {
+//   return (
+//     <div className="w-full relative bg-whitesmoke overflow-hidden flex flex-col items-start justify-start pt-[57px] px-0 pb-0 box-border gap-[35.3px] leading-[normal] tracking-[normal] mq925:gap-[18px]">
+//       <div className="w-[871.8px] h-[191.8px] absolute !m-[0] top-[1708px] right-[-320px] flex items-center justify-center z-[0]">
+//         <img
+//           className="w-full h-full object-contain absolute left-[-165px] top-[0px] [transform:scale(4.449)]"
+//           loading="lazy"
+//           alt=""
+//           src="/group-624.svg"
+//         />
+//       </div>
+//       <FrameComponent9 />
+//       <section className="w-[1693px] flex flex-row items-start justify-start py-0 pr-5 pl-0 box-border relative max-w-full text-center text-[123.2px] text-black font-matter">
+//         <div className="h-[243.4px] w-[1301.8px] absolute !m-[0] right-[81.6px] bottom-[-118.6px] flex items-center justify-center z-[0]">
+//           <img
+//             className="h-full w-full object-contain absolute left-[0px] top-[0px] [transform:scale(2.631)]"
+//             alt=""
+//             src="/group-623.svg"
+//           />
+//         </div>
+//         <div className="flex-1 flex flex-row items-start justify-end pt-0 px-0 pb-[164.6px] box-border max-w-full mq450:pb-[107px] mq450:box-border">
+//           <div className="w-[1463px] flex flex-col items-start justify-start py-0 pr-0 pl-5 box-border gap-[9px] max-w-full">
+//             <b className="self-stretch h-[137.5px] relative tracking-[-0.03em] leading-[100%] flex items-center shrink-0 mq450:text-[31px] mq450:leading-[49px] mq925:text-[49px] mq925:leading-[74px]">
+//               <span>
+//                 <span>{`Explore the `}</span>
+//                 <span className="text-transparent !bg-clip-text [background:linear-gradient(90.97deg,_#ffd60a,_#ac02fc)] [-webkit-background-clip:text] [-webkit-text-fill-color:transparent]">
+//                   differences
+//                 </span>
+//               </span>
+//             </b>
+//             <div className="self-stretch flex flex-row items-start justify-center py-0 pr-[21px] pl-5 box-border max-w-full text-[45.8px] font-inter">
+//               <div className="w-[1128.3px] relative tracking-[-0.03em] leading-[46px] font-medium flex items-center justify-center shrink-0 max-w-full mq450:text-[27px] mq450:leading-[27px] mq925:text-18xl mq925:leading-[37px]">
+//                 Travel freely, anywhere you want.
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+//       <FrameComponent8 />
+//       <section className="self-stretch flex flex-row items-start justify-center pt-0 pb-[125.8px] pr-7 pl-5 box-border max-w-full mq925:pb-[53px] mq925:box-border mq1350:pb-[82px] mq1350:box-border">
+//         <div className="w-[1537px] flex flex-col items-end justify-start gap-[79.9px] max-w-full mq450:gap-[20px] mq925:gap-[40px]">
+//           <FrameComponent7 />
+//           <FrameComponent4 />
+//         </div>
+//       </section>
+//       <FrameComponent3 />
+//       <FrameComponent2 />
+//       <FrameComponent1 />
+//       <FrameComponent />
+//       <section className="self-stretch flex flex-row items-start justify-center py-0 pr-5 pl-[25px] box-border max-w-full text-center text-[46.4px] text-black font-matter">
+//         <div className="w-[660px] flex flex-col items-start justify-start gap-[40.5px] max-w-full mq925:gap-[20px]">
+//           <div className="self-stretch h-[101.5px] relative leading-[120%] font-semibold flex items-center shrink-0 [debug_commit:bf4bc93] mq450:text-9xl mq450:leading-[33px] mq925:text-18xl mq925:leading-[45px]">
+//             <span>
+//               <span>{`Travel Tips and `}</span>
+//               <span className="text-transparent !bg-clip-text [background:linear-gradient(91.81deg,_#ffd60a,_#e434f3_93.42%)] [-webkit-background-clip:text] [-webkit-text-fill-color:transparent]">
+//                 Inspiration
+//               </span>
+//               <span> for Global Explorers</span>
+//             </span>
+//           </div>
+//           <div className="self-stretch flex flex-row items-start justify-center py-0 pr-[22px] pl-5">
+//             <div className="h-[7.7px] w-[81.2px] relative rounded-[48.32px] bg-gold-100 shrink-0 [debug_commit:bf4bc93]" />
+//           </div>
+//         </div>
+//       </section>
+//       <Blog />
+//       <FooterContainer />
+//     </div>
+//   );
+// };
 
-export default Homepage;
+// export default Homepage;
